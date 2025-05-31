8879ac7ca9a1116942473cb0a624c75a806153fd
MariaSolGaray <mariasolgaray01@gmail.com>
2025-05-30 23:41:50 -0300

    Agrego flake8

--- par_api.py
+++ par_api.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
+
 
 def es_par_logico(numero: int) -> bool:
     return numero % 2 == 0
 
+
 @app.get("/")
 def home():
     return {"mensaje": "¡Hola desde FastAPI!"}
+
 
 @app.get("/es_par")
 def es_par(numero: int):
--- test_par.py
+++ test_par.py
@@ -1,5 +1,6 @@
 from par_api import es_par_logico
 
+
 def test_es_par():
-    assert es_par_logico(2) == True
-    assert es_par_logico(3) == False
+    assert es_par_logico(2) is True
+    assert es_par_logico(3) is False
