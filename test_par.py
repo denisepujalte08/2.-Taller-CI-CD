12a172a9483077b641fb04afb119f1177489dfa3
MariaSolGaray <mariasolgaray01@gmail.com>
2025-06-04 22:28:13 -0300

    Arreglar error flake8

--- test_par.py
+++ test_par.py
@@ -2,10 +2,10 @@ from par_api import es_par_logico
 
 
 def test_es_par():
-    assert es_par_logico(2) is True
-    assert es_par_logico(3) is False
-    assert es_par_logico(0) == True
-    assert es_par_logico(-2) == True         # Número negativo par
-    assert es_par_logico(-3) == False        # Número negativo impar
-    assert es_par_logico(1000000) == True    # Número grande par
-    assert es_par_logico(1000001) == False   # Número grande impar
+    assert es_par_logico(2)
+    assert not es_par_logico(3)
+    assert es_par_logico(0)
+    assert es_par_logico(-2)         # Número negativo par
+    assert not es_par_logico(-3)     # Número negativo impar
+    assert es_par_logico(1000000)    # Número grande par
+    assert not es_par_logico(1000001)  # Número grande impar
