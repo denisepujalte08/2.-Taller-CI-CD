cccbf696ed99f9aa2bee8206c6026bafb4870048
MariaSolGaray <mariasolgaray01@gmail.com>
2025-06-07 02:05:16 -0300

    Prueba fallas 5

--- fallas.py
+++ fallas.py
@@ -6,3 +6,24 @@ print("sin indentación")  # <-- mal indentado
 # Romper SonarCloud
 def bug_division():
     return 1 / 0
+
+
+def suma(a, b):
+    return a + b
+
+
+def suma2(a, b):
+    return a + b
+
+
+def no_usada():
+    return "esto nunca se usa"
+
+
+def test_algo():
+    pass
+
+
+def hola():
+    x = 42
+    return "chau"
