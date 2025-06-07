663a26ef89c893fd0dcade9ffb77447f91893003
MariaSolGaray <mariasolgaray01@gmail.com>
2025-06-07 01:37:36 -0300

    Prueba fallas 2

--- fallas.py
+++ fallas.py
@@ -2,9 +2,11 @@
 def foo():
 print("sin indentación")  # <-- mal indentado
 
+
 # Romper SonarCloud
-def bar():
-    if True:
-        return "Esto es un error de SonarCloud"  # <-- mal uso de la condición
-    else:
-        return "Esto no debería ejecutarse"
+def duplicada():
+    return 1
+
+
+def duplicada():
+    return 1
