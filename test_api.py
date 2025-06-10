1c8c83a3a11b6e9e7104ca58022c8c5f22d23a26
MariaSolGaray <mariasolgaray01@gmail.com>
2025-06-10 12:19:04 -0300

    Prueba Coverage Test Sonar

---
+++ test_api.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+from par_api import app
+
+client = TestClient(app)
+
+def test_home():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"mensaje": "¡Hola desde FastAPI!"}
+
+def test_es_par_par():
+    response = client.get("/es_par?numero=2")
+    assert response.status_code == 200
+    assert response.json()["mensaje"] == "El número 2 es par."
+
+def test_es_par_impar():
+    response = client.get("/es_par?numero=3")
+    assert response.status_code == 200
+    assert response.json()["mensaje"] == "El número 3 es impar."
